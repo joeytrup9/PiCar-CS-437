56526f62c285be5b89d4d5d8e9378323f36caaa3
joeytrup9 <joeytrup9@gmail.com>
2022-02-04 14:27:18 -0600

    bug fixed

--- car/picar/env_map.py
+++ car/picar/env_map.py
@@ -5,6 +5,7 @@ from picar_4wd import *
 import time
 from enum import Enum
 from itertools import groupby
+import asciichart as ac
 
 class direction(Enum):
     NORTH = 0
@@ -31,8 +32,8 @@ scan_list = []
     
 
 def reading_distance(sensor_angle, dist):
-    x = dist * np.cos(sensor_angle)
-    y = dist * np.sin(sensor_angle)
+    x = int(dist * np.cos(sensor_angle))
+    y = int(dist * np.sin(sensor_angle))
     return x,y
 
 def offset_point(readingX,readingY):
@@ -60,10 +61,15 @@ def append_coords(sensor_angle,dist):
 
 
 def make_line(x1,y1,x2,y2):
+    if x1 == x2:
+        for y in range(min(y1,y2)+1,max(y1,y2)):
+            pts[int(y)][int(x1)] = 1
+        return
     m =  (y1-y2) / (x1-x2)
+
     for x in range(min(x1,x2)+1,max(x1,x2)):
         y = m * (x - x1) + y1
-        pts[y][x] = 1
+        pts[int(y)][int(x)] = 1
 
 
 
@@ -71,12 +77,11 @@ def connect_coords():
     #for element in list create pairing for each adjacent element with reading != -2 
     global scan_list
     grouping = [list(g) for k, g in groupby(scan_list, lambda x: x != False) if k]
-    
     for g in grouping:
         if len(g) > 1:
             for i in range(len(g)-1):
-                pts[g[i][1]][g[i][0]] = 1
-                pts[g[i+1][1]][g[i+1][0]] = 1
+                pts[int(g[i][1])][int(g[i][0])] = 1
+                pts[int(g[i+1][1])][int(g[i+1][0])] = 1
                 make_line(g[i][0],g[i][1],g[i+1][0],g[i+1][1])
         else:
             pts[g[0][1]][g[0][0]] = 1
@@ -92,7 +97,7 @@ def get_distance_at(angle):
 
 def scan_step():
     global scan_list, current_angle, us_step
-    current_angle += 18
+    current_angle += us_step
     if current_angle >= max_angle:
         current_angle = max_angle
         us_step = -STEP
@@ -101,6 +106,7 @@ def scan_step():
         us_step = STEP
     dist = get_distance_at(current_angle)
     append_coords(current_angle,dist)
+    connect_coords()
     if current_angle == min_angle or current_angle == max_angle:
         if us_step < 0:
             # print("reverse")
@@ -111,5 +117,23 @@ def scan_step():
         return tmp
     else:
         return False
+
+for i in range(100):
+    scan_step()
+
+for i in pts:
+    s = ""
+    for j in i:
+        if j == 1:
+            s += 'x'
+        else:
+            s += '.'
+    print(s)
+
+
+
+
+
+
 
 
