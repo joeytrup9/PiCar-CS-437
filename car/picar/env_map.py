97d341007b6ce6a6d2c354a731c5001f4a4aae51
joeytrup9 <joeytrup9@gmail.com>
2022-02-05 18:02:06 -0600

    working mapping system

--- car/picar/env_map.py
+++ car/picar/env_map.py
@@ -1,7 +1,7 @@
 from ctypes import pointer
 from math import fabs
 import numpy as np
-from picar_4wd import *
+import picar_4wd as fc
 import time
 from enum import Enum
 from itertools import groupby
@@ -16,24 +16,27 @@ class direction(Enum):
 orient = direction.NORTH
 
 ANGLE_RANGE = 180
-STEP = 18
+STEP = 5
 us_step = STEP
 current_angle = 0
 max_angle = ANGLE_RANGE/2
 min_angle = -ANGLE_RANGE/2
  
-N = 100
+N = 50
+US_THRESHOLD = 35
 pts = np.zeros((N,N))
 
-car_x = int(N/2) 
+FULL_SCAN = int(ANGLE_RANGE / STEP) 
+
+car_x = int(N/2)
 car_y = int(N/2)
 
 scan_list = []
     
 
 def reading_distance(sensor_angle, dist):
-    x = int(dist * np.sin(sensor_angle))
-    y = int(dist * np.sin(sensor_angle))
+    x = int(dist * np.sin(sensor_angle * (np.pi / 180)))
+    y = int(dist * np.cos(sensor_angle * (np.pi/180)))
     return x,y
 
 def offset_point(readingX,readingY):
@@ -54,18 +57,23 @@ def append_coords(sensor_angle,dist):
         return False
     readingX, readingY = reading_distance(sensor_angle,dist)
     point_x,point_y = offset_point(readingX,readingY)
-    if not (point_x < 0 or point_x > N or point_y < 0 or point_y > N):
+    point_x -= 1
+    point_y -= 1
+    if not (point_x < 0 or point_x > N-1 or point_y < 0 or point_y > N-1 or dist > US_THRESHOLD):
         scan_list.append((point_x,point_y))
+    else:
+        scan_list.append(False)
     return point_x,point_y
 
 
 
 def make_line(x1,y1,x2,y2):
+    print(x1,y1,x2,y2)
     if x1 == x2:
         for y in range(min(y1,y2)+1,max(y1,y2)):
             pts[int(y)][int(x1)] = 1
         return
-    m =  (y1-y2) / (x1-x2)
+    m =  (y2-y1) / (x2-x1)
 
     for x in range(min(x1,x2)+1,max(x1,x2)):
         y = m * (x - x1) + y1
@@ -77,6 +85,8 @@ def connect_coords():
     #for element in list create pairing for each adjacent element with reading != -2 
     global scan_list
     grouping = [list(g) for k, g in groupby(scan_list, lambda x: x != False) if k]
+    print(grouping)
+    
     for g in grouping:
         if len(g) > 1:
             for i in range(len(g)-1):
@@ -90,14 +100,16 @@ def connect_coords():
             
 
 def get_distance_at(angle):
-    servo.set_angle(angle)
-    time.sleep(0.04)
-    distance = us.get_distance()
+    print(angle)
+    fc.servo.set_angle(angle)
+    time.sleep(0.02)
+    distance = fc.us.get_distance()
     return distance
 
 def scan_step():
     global scan_list, current_angle, us_step
     current_angle += us_step
+    
     if current_angle >= max_angle:
         current_angle = max_angle
         us_step = -STEP
@@ -106,9 +118,10 @@ def scan_step():
         us_step = STEP
     dist = get_distance_at(current_angle)
     append_coords(current_angle,dist)
-    connect_coords()
+    print(current_angle,dist) 
     if current_angle == min_angle or current_angle == max_angle:
-        if us_step < 0:
+        connect_coords()
+        if us_step < 0: 
             # print("reverse")
             scan_list.reverse()
         # print(scan_list)
@@ -118,22 +131,33 @@ def scan_step():
     else:
         return False
 
-for i in range(100):
-    scan_step()
+def single_full_scan():
+    global current_angle
+    fc.servo.set_angle(-90)
+    time.sleep(.5)
+    current_angle = -90
+    for i in range(FULL_SCAN):
+        scan_step()
 
-for i in pts:
-    s = ""
-    for j in i:
-        if j == 1:
-            s += 'x'
-        else:
-            s += '.'
-    print(s)
+def print_readable():
+    for i in np.flipud(pts):
+        s = ""
+        for j in i:
+            if j == 1:
+                s += 'X'
+            elif j == 2:
+                s += 'O'
+            else:
+                s += '.'
+        print(s)
+
+def test1():
+    single_full_scan()
 
 
 
+def test2():
+    make_line(0,0,50,50)
 
-
-
-
-
+test1()
+print_readable()
